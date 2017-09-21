2af960eb117f1d8f9096a9cfdc4dd8063c170e00
Colin Richardson <richardsonjcolin@gmail.com>
2017-09-21 18:07:16 +0000

    Adds the last exercises [completed]

---
+++ functions-arguments.js
@@ -0,0 +1,4 @@
+function math(additionNum, firstMultiplicationNum, secondMultiplicationNum) {
+  return firstMultiplicationNum * secondMultiplicationNum + additionNum;
+}
+console.log(math(53, 61, 67));
